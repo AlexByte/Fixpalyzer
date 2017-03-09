6294cbec05d3fd390a2d45fd64130366c6c75bca
AlexByte <f.d22016@yandex.ru>
2017-03-10 01:31:44 +0300

    Revert "upd"

--- src/drivers/phantomjs/driver.js
+++ src/drivers/phantomjs/driver.js
@@ -88,7 +88,7 @@
 
 						apps.push({
 							name: app,
-							confidence: wappalyzer.detected[url][app].confidenceTotal.toString(),
+							confidence: wappalyzer.detected[url][app].confidenceTotal,
 							version:    wappalyzer.detected[url][app].version,
 							icon:       wappalyzer.apps[app].icon,
 							categories: cats
@@ -132,6 +132,10 @@
 				page.settings.loadImages      = false;
 				page.settings.userAgent       = 'Mozilla/5.0 (compatible; Wappalyzer; +https://github.com/AliasIO/Wappalyzer)';
 				page.settings.resourceTimeout = resourceTimeout;
+
+				page.onConsoleMessage = function(message) {
+					require('system').stdout.write(message + "\n");
+				};
 
 				page.onError = function(message) {
 					wappalyzer.log(message, 'error');
